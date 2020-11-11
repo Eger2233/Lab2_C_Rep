91eb9813635e928355d499fff00452a484803cac
Eger2233 <funtik4433@gmail.com>
2020-11-11 19:01:52 +0300

    Initial commit

---
+++ src/Pack/MainApplication.java
@@ -0,0 +1,14 @@
+package Pack;
+
+import javax.swing.JFrame;
+
+public class MainApplication
+{
+    public static void main(String[]args)
+    {
+        Main_Frame frame = new Main_Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
